87b9d2efaad11ffd82e3976ac8193dc753ae183f
chem <di4ems@gmail.com>
2018-06-04 17:06:00 +0300

    disable slice unselection

--- MPChartLib/src/main/java/com/github/mikephil/charting/listener/ChartTouchListener.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/listener/ChartTouchListener.java
@@ -118,8 +118,8 @@ public abstract class ChartTouchListener<T extends Chart<?>> extends GestureDete
     protected void performHighlight(Highlight h, MotionEvent e) {
 
         if (h == null || h.equalTo(mLastHighlighted)) {
-            mChart.highlightValue(null, true);
-            mLastHighlighted = null;
+//            mChart.highlightValue(h, true);
+//            mLastHighlighted = null;
         } else {
             mChart.highlightValue(h, true);
             mLastHighlighted = h;
